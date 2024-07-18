cea577df2b24e6353a2bb17e5ce64fe037fcc7b8
AbhiBondarde <abondarde11@gmail.com>
2024-07-18 11:43:14 +0530

    commit

---
+++ 01-Basics/01_Variables.js
@@ -0,0 +1,19 @@
+const accountID = 144534
+let accountEmail =  "ab@google.com"
+var accountPass = "12354"
+
+// This is not right way to use
+accountCity =  "Jaipur" 
+
+
+// If not initialized default value is undefined
+let accountState;  
+
+console.log(accountID);
+
+console.table([accountID,accountPass,accountEmail,accountCity,accountState]);
+
+
+/*  var  -> prefer not to use var
+ 			becoz of issue in block scope and fuctional scope
+ */           
---
+++ 01-Basics/02_DataTypes.js
@@ -0,0 +1,16 @@
+/*  Primitive Data Types
+	1)String => ""
+	2)number, bigInt
+	3)boolean -> true/false
+	4)null -> Standalone Value
+	5)undefined -> value not assign
+	6)symbol ->unique 	
+  */  
+
+
+//  Object Data Type	
+console.log(typeof "abhi") // String
+
+console.log(typeof null) // Object
+
+console.log(typeof undefined) // Undefined
