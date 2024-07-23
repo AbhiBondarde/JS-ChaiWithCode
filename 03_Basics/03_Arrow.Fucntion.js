50cf9fe96014c7fa5f24aa575730fed3c61a497a
AbhiBondarde <abondarde11@gmail.com>
2024-07-23 10:09:34 +0530

    commit

--- 03_Basics/03_Arrow.Fucntion.js
+++ 03_Basics/03_Arrow.Fucntion.js
@@ -1,21 +1,52 @@
-function chai()
-{
-   //console.log(this); 
-}
-
-// chai()
-
-
 const user ={
     userName:"ABhi",
     city:"Nashik",
 
     welcome :function ()
     {
-        console.log(`${user.userName},Welocme `);
-        console.log(this)
+        console.log(`${this.userName},Welocme `);
+        // console.log(this)
     }
 
 }
-user.welcome()
+// user.welcome()
 
+// function chai()
+// {
+//     let userName="sfsfsfs"
+//    console.log(this.userName); 
+// }
+
+// const chai =function(){
+//     let userName="sfsfsfs"
+//     console.log(this.userName); 
+// }
+
+ 
+// ***************************  Arrow Function ***************************
+
+const chai = () => {
+    let userName="chaiii"
+    console.log(this);
+}
+
+// chai()
+
+const addTwo = (num1, num2) => {
+        return num1+num2
+}
+
+console.log(addTwo(224,6));
+
+// Implicitly Return  Arrow Function
+// no return keword, no {} curley braces
+// compulsory use () paranthesis
+const square = (num) => (num*num)
+console.log(square(5));
+
+
+// USe object in arrow function
+// compulsory object use inside ()parenthesis
+const anyFunction = () => ({pincode:43433})
+
+console.log(anyFunction());
